5d71853fa39af2500d87913d1b98e2243c49ebec
AlexandruCalinica <calinicaalexandru@gmail.com>
2023-05-15 16:04:18 +0300

    add vercel adaptor for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+  ],
+  adapter: vercel({
+    analytics: true,
+  }),
+  site: `https://www.entity-of.xyz`,
 });

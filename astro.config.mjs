9cc1453b2d49989647711fb773fa2872fd98299a
AlexandruCalinica <calinicaalexandru@gmail.com>
2023-05-16 18:36:28 +0300

    add astro sitemap extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,17 @@ import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/static";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), sitemap()],
   adapter: vercel({
-    analytics: true,
+    analytics: true
   }),
-  site: `https://www.entity-of.xyz`,
+  site: `https://www.entity-of.xyz`
 });
